a5c65d4385c655bc37eeeb79edf4791bd2105422
Tynybek <tynybeko1122@gmail.com>
2024-02-06 23:18:22 +0600

    asdsad

--- math.test.js
+++ math.test.js
@@ -1,8 +1,8 @@
 const { add } = require('./index.js');
 
-test('add function adds two numbers correctly', () => {
+test('Наш тестовый тест)', () => {
     expect(add(1, 2)).toBe(3);
-    expect(add(-1, 1)).toBe(0);
+    expect(add(-1, 1)).toBe(1);
     expect(add(0, 0)).toBe(0);
 });
 
