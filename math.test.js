85d9e15c7efc9e6f4e6694b9483a2e1b3301be87
Tynybek <tynybeko1122@gmail.com>
2024-02-10 19:47:47 +0600

    add

--- math.test.js
+++ math.test.js
@@ -2,7 +2,7 @@ const { add } = require('./index.js');
 
 test('Наш тестовый тест)', () => {
     expect(add(1, 2)).toBe(3);
-    expect(add(-1, 1)).toBe(1);
+    expect(add(-1, 1)).toBe(0);
     expect(add(0, 0)).toBe(0);
 });
 
